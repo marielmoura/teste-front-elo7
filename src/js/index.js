741f719a3587f47bddb2c774c01611d057d8e751
Thiago Fogolin <thiago.fogolin@gmail.com>
2020-12-02 18:43:44 -0300

    Mobile and js request

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+   
+
+    fetch('http://www.mocky.io/v2/5d6fb6b1310000f89166087b')
+    .then(res=>res.json()).then(res=>res.vagas)
+    .then(vagas=>vagas.filter(vaga=>vaga.ativa==true))
+    .then(vagas=>populateList(vagas))
+    .catch(err => console.log(err))
+ 
+});
+function populateList(vagas){
+    let positionsList = document.getElementById("positions")
+
+    vagas.forEach(vaga => {
+        console.log(vaga)
+        let li = document.createElement("li");
+        let location = document.createElement("span.location");
+        li.textContent = vaga.cargo;
+        location.textContent = vaga.localizacao?vaga.localizacao.bairro + ' - ' + vaga.localizacao.cidade:"Remoto"
+        li.appendChild(location)
+
+        positionsList.appendChild(li);
+        
+    });
+
+}
+ 
